0107da377771a360c055fd1764174d9a840767d5
Rex Posadas <rexposadas@gmail.com>
2023-03-26 22:00:48 +0800

    Squashed commit of the following:
    commit cae128018fc528350831c21e95e5ee1c75a69d9e
    Author: Rex Posadas <rexposadas@gmail.com>
    Date:   Sun Mar 26 22:00:34 2023 +0800
    
        added dalle2 command

---
+++ cmd/dalle2.go
@@ -0,0 +1,90 @@
+package cmd
+
+import (
+	"bytes"
+	"context"
+	"encoding/base64"
+	"fmt"
+	"github.com/rexposadas/go-openai-cli/cmd/util/require"
+	"github.com/sashabaranov/go-openai"
+	"image/png"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var dalle2Cmd = &cobra.Command{
+	Use:   "dalle2",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		token := require.APIToken()
+		c := openai.NewClient(token)
+		ctx := context.Background()
+
+		// Sample image by link
+		reqUrl := openai.ImageRequest{
+			Prompt:         "Parrot on a skateboard performs a trick, cartoon style, natural light, high detail",
+			Size:           openai.CreateImageSize256x256,
+			ResponseFormat: openai.CreateImageResponseFormatURL,
+			N:              1,
+		}
+
+		respUrl, err := c.CreateImage(ctx, reqUrl)
+		if err != nil {
+			fmt.Printf("Image creation error: %v\n", err)
+			return
+		}
+		fmt.Println(respUrl.Data[0].URL)
+
+		// Example image as base64
+		reqBase64 := openai.ImageRequest{
+			Prompt:         "half human half machine, high detail, realistic light, unreal engine",
+			Size:           openai.CreateImageSize256x256,
+			ResponseFormat: openai.CreateImageResponseFormatB64JSON,
+			N:              1,
+		}
+
+		respBase64, err := c.CreateImage(ctx, reqBase64)
+		if err != nil {
+			fmt.Printf("Image creation error: %v\n", err)
+			return
+		}
+
+		imgBytes, err := base64.StdEncoding.DecodeString(respBase64.Data[0].B64JSON)
+		if err != nil {
+			fmt.Printf("Base64 decode error: %v\n", err)
+			return
+		}
+
+		r := bytes.NewReader(imgBytes)
+		imgData, err := png.Decode(r)
+		if err != nil {
+			fmt.Printf("PNG decode error: %v\n", err)
+			return
+		}
+
+		file, err := os.Create("example.png")
+		if err != nil {
+			fmt.Printf("File creation error: %v\n", err)
+			return
+		}
+		defer file.Close()
+
+		if err := png.Encode(file, imgData); err != nil {
+			fmt.Printf("PNG encode error: %v\n", err)
+			return
+		}
+
+		fmt.Println("The image was saved as example.png")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(dalle2Cmd)
+}
